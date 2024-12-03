cc3a4dbc9dd448d8e3d716e3cb95861760861ecf
reynaroyce12 <reynaroyce.12@gmail.com>
2024-12-03 01:25:17 -0600

    initial

---
+++ buyAndSellStocks.py
@@ -0,0 +1,17 @@
+def buy_and_sell_stocks():
+    prices = [2,4,1]
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+
+        print(profit)
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
+
+print(buy_and_sell_stocks())
