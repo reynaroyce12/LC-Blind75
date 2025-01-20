8603bee23f985fd0ca5da1112a70d18b68b35efc
reynaroyce12 <reynaroyce.12@gmail.com>
2025-01-19 22:16:20 -0600

    Added longest palindrome solution

---
+++ Strings/Palindromic_Substrings/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        valid_palindromes = 0
+
+        def expand(left, right):
+            nonlocal valid_palindromes
+            while left >= 0 and right < len(s):
+                if s[left] == s[right]:
+                    valid_palindromes += 1
+                    left -= 1
+                    right += 1
+                else:
+                    break
+
+        for i in range(len(s)):
+            expand(i, i) # For odd length palindrome
+            expand(i, i + 1) # For even length palindrome
+
+        return valid_palindromes
+
+
+solution = Solution()
+s = "abc"
+result = solution.countSubstrings(s)
+print(result)
